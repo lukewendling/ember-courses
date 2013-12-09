a53b3ae8b42c27f18bc9654a53d10f61082f51d6
Luke Wendling <luke@lukewendling.com>
2013-12-09 14:25:01 -0600

    create Ember app

---
+++ public/app/application.js
@@ -0,0 +1,15 @@
+var App = Ember.Application.create({
+  LOG_TRANSITIONS_INTERNAL: true
+});
+
+App.RESTAdapter = RL.RESTAdapter.create({
+  url: 'http://localhost:3000'
+});
+// App.ApplicationAdapter = App.RESTAdapter;
+App.Client = RL.Client.create({
+  adapter: App.RESTAdapter
+});
+
+Ember.RSVP.configure('onerror', function(error) {
+  Ember.Logger.assert(false, error);
+});
