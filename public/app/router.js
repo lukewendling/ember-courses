5ce9e500f9b0d9a7d0004a609e02bd19625cda96
Luke Wendling <luke@lukewendling.com>
2013-12-11 17:06:06 -0600

    minor UI mods

--- public/app/router.js
+++ public/app/router.js
@@ -1,7 +1,7 @@
 App.Router.map(function () {
   this.route('login');
-  this.resource('chapters', { path: '/' }, function () {
-    this.resource('chapter', { path: '/chapters/:chapter_id' }, function () {
+  this.resource('chapters', function () {
+    this.resource('chapter', { path: '/:chapter_id' }, function () {
       this.resource('lessons', function () {
         this.resource('lesson', { path: '/:lesson_id' }, function () {
         });
